d5602968e27503270f10f062fdfee393ceabc34d
janu01ary <janu01ary@gmail.com>
2021-07-21 01:30:26 +0900

    add static image

--- imgproject/urls.py
+++ imgproject/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('create', imgapp.views.create, name='create'),
     path('update/<int:id>', imgapp.views.update, name='update'),
     path('delete/<int:id>', imgapp.views.delete, name='delete'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
